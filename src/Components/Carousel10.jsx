cc2f5ec4229cd7a34e6d325b7c88c7035bbe08f7
v-pawar <165990389+v-pawar@users.noreply.github.com>
2025-09-20 22:39:38 +0530

    Add files via upload

---
+++ src/Components/Carousel10.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Carousel10() {
+    return (
+        <div>
+            <div className="bg-white  px-2 mt-4 max-w-[70rem] mx-auto h-[174px] ">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {/* Box 1 */}
+                    <div className="rounded-sm overflow-hidden">
+                        <img
+                            src="/img/Carousel24.jpg"
+                            alt="Box 1"
+                            className="w-full h-[173px] object-fit"
+                        />
+                    </div>
+
+                    {/* Box 2 */}
+                    <div className="rounded-sm overflow-hidden">
+                        <img
+                            src="/img/Carousel25.jpg"
+                            alt="Box 2"
+                            className="w-full h-[173px] object-fit"
+                        />
+                    </div>
+
+                    {/* Box 3 */}
+                    <div className="rounded-sm overflow-hidden">
+                        <img
+                            src="/img/Carousel26.jpg"
+                            alt="Box 3"
+                            className="w-full h-[173px] object-fit"
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Carousel10
